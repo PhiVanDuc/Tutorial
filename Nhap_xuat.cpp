55625a0c902043c1c904d7431d1c3f282f7dbacf
PhiVanDuc <phivanduc325@gmail.com>
2022-09-03 10:48:09 +0700

    initial commit

---
+++ Nhap_xuat.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Nhap n: "; cin >> n;
+	return 0;
+}
+
---
+++ TinhTong.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 3, b = 2;
+	cout << "a + b = " << a + b;
+	return 0;
+}
+
