3630977509bf3767724933170764acf174ffadf3
PhiVanDuc <phivanduc325@gmail.com>
2022-09-03 11:55:47 +0700

    Add Socp.cpp

---
+++ Socp.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+bool socp(int x)
+{
+	int kc = sqrt(x);
+	if(pow(kc, 2) != x)
+		return false;
+	return true;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	if(socp(n) == true)
+		cout << "Dung";
+	else
+		cout << "Sai";
+	return 0;
+}
+
